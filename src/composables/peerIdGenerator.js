19fbe142a516d765728517e0b06a390c73cd0299
Remi-dee <oluwaremidaniel36@gmail.com>
2023-05-15 15:07:19 -0700

    clean code and export generatePeerId as a named export

--- src/composables/peerIdGenerator.js
+++ src/composables/peerIdGenerator.js
@@ -1,35 +1,11 @@
-//A 10-character peer ID generator
-
-function generatePeerIdCharacter(number) {
+export const generatePeerIdCharacter = () => {
   let characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const charac = characters.length;
 
   let peerId = "";
 
-  for (let i = 0; i < number; i++)
-    peerId += characters.charAt(Math.floor(Math.random() * charac));
+  for (let i = 0; i < 10; i++)
+    peerId += characters.charAt(Math.floor(Math.random() * characters.length));
 
-  //Check if generated peerId exists
-  if (idAlreadyExists(peerId)) {
-    console.log("oops! id already exists!");
-    // id exist? generate a new peerId recursively
-    return generatePeerIdCharacter(number);
-  }
-
-  console.log(`Hi anonymousauthority, here you go! your peerId is ${peerId}`);
-}
-
-// Ensures not to generate existing id
-function idAlreadyExists(peerId) {
-  /*
-      Case - id is stored in DB? 
-          res = query database to find id by (peerId)
-          if(res) return true
-      
-      */
-  return false;
-}
-
-//Call the function below to generate a unique peerId
-generatePeerIdCharacter(10);
+  return peerId;
+};
