69066ab652b8a5aed0accb7567318be221bfe325
Chidera <kharunaking@gmail.com>
2023-05-16 12:26:03 -0400

    Update

--- VerificationCounterFunction/RequestVerificationLink.js
+++ VerificationCounterFunction/RequestVerificationLink.js
@@ -1,22 +1,12 @@
-const requestVerificationLink = () => {
-  // Disable the button to prevent multiple requests
-  document.getElementById("requestLinkButton").disabled = true;
-
-  // Set the initial count
-  let count = 120;
-
-  // Update the button text and countdown every second
-  const countdown = setInterval(() => {
-    count--;
-    document.getElementById(
-      "requestLinkButton"
-    ).innerText = `Request Link (${count}s)`;
-
-    // If the countdown reaches 0, re-enable the button
-    if (count === 0) {
-      clearInterval(countdown);
-      document.getElementById("requestLinkButton").disabled = false;
-      document.getElementById("requestLinkButton").innerText = "Request Link";
+const startCounter = () => {
+  let counter = 120;
+  const intervalID = setInterval(() => {
+    counter--;
+    if (counter == 0) {
+      clearInterval(intervalID);
+      return true;
     }
   }, 1000);
+  return false;
 };
+startCounter();
