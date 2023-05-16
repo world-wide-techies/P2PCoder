34a55aaca8593be2b08f319ea57124531a6e0fff
Oladipo <is.haqomotosho@gmail.com>
2023-05-16 20:31:46 +0100

    Created a function to reset Account using firebase

--- src/composables/resetAccountWithFirebase.js
+++ src/composables/resetAccountWithFirebase.js
@@ -1,18 +1,18 @@
-import { sendPasswordResetEmail } from "firebase/auth";
-import { appAuth } from "./firebaseConfig/config"
+import { updatePassword } from "firebase/auth";
+import { appAuth } from "./firebaseConfig/config";
 
+export function resetUserAccount(email, newPassword) {
+    const auth = appAuth();
 
-const auth = appAuth();
-sendPasswordResetEmail(auth, email)
-    .then(() => {
-        console.log("Password reset email sent!");
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error("Password reset failed!", errorCode, errorMessage);
-    });
-
-
-
-
+    auth.sendPasswordResetEmail(email)
+        .then(() => {
+            const user = auth.currentUser;
+            return updatePassword(user, newPassword);
+        })
+        .then(() => {
+            console.log("User account reset successfully.");
+        })
+        .catch((error) => {
+            console.error("Failed to reset user account:", error);
+        });
+}
