464094b3b73ec7289e1663d82fbdb6a04a494a34
Oladipo <is.haqomotosho@gmail.com>
2023-05-16 21:36:47 +0100

    Created a function to reset Account using firebase

--- src/composables/resetAccountWithFirebase.js
+++ src/composables/resetAccountWithFirebase.js
@@ -1,13 +1,18 @@
 import { sendPasswordResetEmail } from "firebase/auth";
 import appAuth from "./firebaseConfig/config";
 
-sendPasswordResetEmail(appAuth(), email)
-    .then(() => {
-        // Password reset email sent!
-        // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ...
-    });
+
+
+export function resetAccountWithFirebase() {
+    sendPasswordResetEmail(appAuth(), email)
+        .then(() => {
+            // Password reset email sent!
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ...
+        });
+
+}
