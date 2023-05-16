069ebf96bed71b16d0b75401055406b35df05017
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-05-16 21:05:16 +0400

    Login Function For User Login Page- recently corrected

--- src/composables/userLoginFunction.js
+++ src/composables/userLoginFunction.js
@@ -1,17 +1,18 @@
 import { signInWithEmailAndPassword } from "firebase/auth"
-import  {appAuth}  from "./firebaseConfig/config"
+import  { appAuth }  from "./firebaseConfig/config"
 
 
 async function UserLogin(emailAddress, password){
     try {
     const userInfo = await signInWithEmailAndPassword(appAuth, emailAddress, password)
     const user = userInfo.user;
-    console.log('Login successful', user)
+    if (user) {
+        return { LoggedIn: true, user };
+    }
     } 
     catch (error) {
-        const errorCode = error.code
         const errorMessage = error.message
-        console.log('Login error:', errorCode, errorMessage)
+        return { LoggedIn: false, message: errorMessage }
     }
 }  
 
