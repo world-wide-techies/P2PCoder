0b1ad36e67f1c0e689a0a6f3db7ee2e6537e0d07
Chidera <kharunaking@gmail.com>
2023-05-15 21:04:36 -0400

    initial commit

---
+++ ResendVerificationCounter/READme.md
@@ -0,0 +1,12 @@
+#Nextjs
+#Resend Verification Counter Function
+This function allows users to request for another verification link after the counter interval
+
+
+Deployed link for the Survey Form
+
+Welcome! 👋
+
+I love receiving feedback! I'm always looking to improve my challenges. So if you have anything you'd like to mention, please do so!
+
+Have fun building! 🚀
---
+++ ResendVerificationCounter/VerificationCounter.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+const VerificationCounter = ({ onTimerComplete }) => {
+  const [secondsRemaining, setSecondsRemaining] = useState(120);
+
+  useEffect(() => {
+    let timer = null;
+
+    if (secondsRemaining > 0) {
+      timer = setTimeout(() => {
+        setSecondsRemaining((prevSeconds) => prevSeconds - 1);
+      }, 1000);
+    } else {
+      onTimerComplete();
+    }
+
+    return () => clearTimeout(timer);
+  }, [secondsRemaining, onTimerComplete]);
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
+  };
+
+  return (
+    <div>
+      {secondsRemaining > 0 ? (
+        <p>
+          Request a new verification link in {formatTime(secondsRemaining)}{" "}
+          seconds.
+        </p>
+      ) : (
+        <p>
+          Request a new verification link now.{" "}
+          {/* Replace this with your desired button or link */}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default VerificationCounter;
