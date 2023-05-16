d7a3bd0c0277ed0e9d488c45230b32f956798e22
Taylor J <taylordjone@gmail.com>
2023-05-16 12:57:32 +0200

    updating function

--- src/composables/authSignupFunction.js
+++ src/composables/authSignupFunction.js
@@ -1,26 +1,22 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { appAuth } from "@/firebase/config";
-import { useState } from "react";
 
-function useAuthSignUp() {
-  const [addUser, setAddUser] = useState("");
-
-  const signup = ({ name, email, password }) => {
-    createUserWithEmailAndPassword(appAuth, email, password)
-      .then((userCredential) => {
-        updateProfile(userCredential.user, { displayName: name });
-      })
-      .then((userCredential) => {
-        const user = userCredential.user;
-        setAddUser(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
-  };
-
-  return { addUser, signup };
+async function authSignUp(name, email, password) {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      appAuth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    await updateProfile(user, { displayName: name });
+    return user;
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    throw new Error(errorMessage);
+  }
 }
 
-export { useAuthSignUp };
+const user = await authSignUp("Jane Doe", "example@example.com", "password123");
+console.log(user);
