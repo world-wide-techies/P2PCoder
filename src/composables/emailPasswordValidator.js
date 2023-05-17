2fc2a07974a972654662eb408d99531c3da035df
Deborah <deborahihesiaba@gmail.com>
2023-05-17 09:58:54 +0100

    Seperated  the function into email and password

--- src/composables/emailPasswordValidator.js
+++ src/composables/emailPasswordValidator.js
@@ -1,16 +1,14 @@
-function emailPasswordValidator(email, password) {
+function emailValidator(email) {
   var regxEmail = /^([a-zA-Z0-9\.]+)@([a-zA-Z]+)\.([a-z]+)(\.[a-z]+)?$/;
 
+  return regxEmail.test(email) ? true : "Please enter a valid email address";
+}
+
+function passwordValidator(password) {
   var regxPassword =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
 
-  regxEmail.test(email)
-    ? console.log("Valid email address!")
-    : console.log("Please enter a valid email address");
-
-  regxPassword.test(password)
-    ? console.log("Valid password")
-    : console.log("Invalid password");
+  return regxPassword.test(password) ? true : "Please enter a valid password";
 }
 
-export { emailPasswordValidator };
+export { emailValidator, passwordValidator };
