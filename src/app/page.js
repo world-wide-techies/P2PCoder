c6441e74a6f2c5f475ba035856c66d64f6d67d9b
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-05-18 15:07:06 +0400

    Login screen component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 "use client";
 import { OnboardingHeader } from "@/components/onboardingHeader";
+import UserLoginComp from "@/components/userLogin_comp";
 import React from "react";
 import { useEffect, useState } from "react";
+
 
 function Home() {
   const [theme, setTheme] = useState(
@@ -25,7 +27,7 @@ function Home() {
           : "flex items-center justify-center h-screen w-full "
       }
     >
-      <div className="w-1/3">
+       <div className="w-1/3">
         <OnboardingHeader
           h1={"Create an Account with us"}
           p={"Where your world becomes a better place"}
@@ -33,7 +35,8 @@ function Home() {
       </div>
       <button type="button" onClick={handleThemeChange}>
         Change Theme
-      </button>
+      </button> 
+      
     </div>
   );
 }
