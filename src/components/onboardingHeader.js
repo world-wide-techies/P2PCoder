282ce91571bbe55a63dd7b4ae9346b5f278131f9
Deborah <deborahihesiaba@gmail.com>
2023-05-18 13:13:16 +0100

    Implemented dark theme in onboardingHeader component

--- src/components/onboardingHeader.js
+++ src/components/onboardingHeader.js
@@ -1,8 +1,12 @@
 export const OnboardingHeader = ({ h1, p }) => {
   return (
     <div>
-      <h1 className="text-violet-800 font-extrabold text-3xl mb-3">{h1}</h1>
-      <p className=" text-neutral-800 font-medium text-sm">{p}</p>
+      <h1 className="text-violet-800 font-extrabold text-3xl mb-3 dark:text-violet-200">
+        {h1}
+      </h1>
+      <p className=" text-neutral-800 font-medium text-sm   dark:text-neutral-200">
+        {p}
+      </p>
     </div>
   );
 };
