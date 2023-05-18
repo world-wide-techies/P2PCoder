ce7b6e407d02e72a3db8878978ce41a92c2bac43
Remi-dee <oluwaremidaniel36@gmail.com>
2023-05-18 10:41:01 -0700

    integrate font assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        nohemi: ["var(--font-nohemi)"]
+      },
     },
   },
   plugins: [],
