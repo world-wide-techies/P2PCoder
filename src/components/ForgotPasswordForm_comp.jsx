a972708c979f65e94bf8d238e67808b096009cf4
Oladipo <is.haqomotosho@gmail.com>
2023-05-19 15:42:18 +0100

    Created a view component for the forgotPassword form

--- src/components/ForgotPasswordForm_comp.jsx
+++ src/components/ForgotPasswordForm_comp.jsx
@@ -5,7 +5,7 @@ const ForgotPassword_comp = () => {
   return (
     <div className="w-full flex flex-col justify-center items-start bg-[#F3F3F6] rounded-3xl shadow-lg">
       <span>
-        <h3 className="text-blue-500 sm:text-lg ">Forgot Password</h3>
+        <h3 className="text-blue-500 sm:text-lg font-bold ">Forgot Password</h3>
         <Image src={reset} alt="reset" />
       </span>
       <p className="text-base leading-5 text-[#0E0C46] font-normal">
