188a3c4cd04eef4e9e1359f4861270942922b19a
Taylor J <taylordjone@gmail.com>
2023-05-19 15:24:55 +0200

    creating signup component

---
+++ src/components/signup_comp.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { OnboardingHeader } from "./onboardingHeader";
+import Link from "next/link";
+
+function SignUpComponent() {
+  return (
+    <form className="space-y-5">
+      <OnboardingHeader
+        h1={"Create an account with us"}
+        p={"Enjoy extra features when you create an account with us."}
+      />
+
+      <div className="flex justify-between items-center space-x-3">
+        <button className="w-1/2 p-3 bg-gray-200 rounded-lg shadow-lg flex">
+          <span>Create account with Google</span>
+        </button>
+        <button className="w-1/2 p-3 bg-gray-200 rounded-lg shadow-lg flex">
+          <span>Create account with Github</span>
+        </button>
+      </div>
+
+      <div className="flex items-center text-center">
+        <div className="border-b-2 border-gray-300 w-full relative flex justify-center"></div>
+        <p className="text-black w-1/6">OR</p>
+        <div className="border-b-2 border-gray-300 w-full relative flex justify-center"></div>
+      </div>
+
+      {/* Signup Form */}
+      <div className="flex flex-wrap justify-between items-center ">
+        <div className="flex flex-col w-1/2">
+          <label>First Name</label>
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            placeholder="Enter First Name"
+            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
+          />
+        </div>
+        <div className="flex flex-col w-1/2">
+          <label>Email Address</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Enter Email Address"
+            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
+          />
+        </div>
+
+        <div className="flex flex-col w-1/2">
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter User Name"
+            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
+          />
+        </div>
+
+        <div className="flex flex-col w-1/2">
+          <label>Last Name</label>
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            placeholder="Enter Last Name"
+            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
+          />
+        </div>
+
+        <div className="flex flex-col w-1/2">
+          <label>Password*</label>
+          <input
+            type="text"
+            name="password"
+            id="password"
+            placeholder="Enter Password"
+            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
+          />
+        </div>
+
+        <div className="flex flex-col w-1/2">
+          <label>Confirm Password*</label>
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            placeholder="Re-enter Password"
+            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
+          />
+        </div>
+      </div>
+
+      {/* Form Footer */}
+      <div className="space-y-3">
+        <Link
+          href="/"
+          className="bg-violet-800 text-white text-center font-bold block w-full p-3 rounded-md"
+        >
+          Create Account
+        </Link>
+
+        <p className="text-center font-semibold ">
+          Already have an account?
+          <Link href="/" className="text-violet-800 ml-1.5">
+            Log in
+          </Link>
+        </p>
+      </div>
+    </form>
+  );
+}
+
+export default SignUpComponent;
