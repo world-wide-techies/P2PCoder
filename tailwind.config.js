3f7ced2d9f3f684373eda60b649eacc52fb833d8
Remi Daniel <96704300+Remi-dee@users.noreply.github.com>
2023-05-19 23:49:16 +0100

    Update tailwind.config.js
    Removed unused font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,6 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        sans: ["var(--font-inter)"],
         nohemi: ["var(--font-nohemi)"]
       },
     },
