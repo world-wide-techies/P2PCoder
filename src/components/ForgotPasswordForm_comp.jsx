ff9da26c43b99d636f957906f2c81c6190149337
Oladipo <is.haqomotosho@gmail.com>
2023-05-19 16:25:58 +0100

    Created a view component for the forgotPassword form

--- src/components/ForgotPasswordForm_comp.jsx
+++ src/components/ForgotPasswordForm_comp.jsx
@@ -3,17 +3,17 @@ import reset from "../assets/Icons/Light Mode Icon/reset.png";
 import Image from "next/image";
 const ForgotPassword_comp = () => {
   return (
-    <div className="w-full flex flex-col justify-center items-start bg-[#F3F3F6] rounded-3xl shadow-lg">
+    <div className="w-full flex flex-col justify-center items-start bg-white rounded-3xl shadow-lg">
       <span>
         <h3 className="text-blue-500 sm:text-lg font-bold ">Forgot Password</h3>
         <Image src={reset} alt="reset" />
       </span>
-      <p className="text-base leading-5 text-[#0E0C46] font-normal">
+      <p className="text-base leading-5 text-blue-950 font-normal">
         Enter your email address to reset your password
       </p>
       <form>
         <label
-          className="text-base leading-5 text-[#0E0C46] font-normal"
+          className="text-base leading-5 text-blue-950 font-normal"
           htmlFor=""
         >
           Email Address
@@ -21,9 +21,9 @@ const ForgotPassword_comp = () => {
         <input
           type="text"
           placeholder="Enter Email Address"
-          className="bg-[#EBEBF0] rounded border-1 border-[#DCDCE5]"
+          className="bg-neutral-50 rounded border-1 border-gray-400"
         />
-        <button className="text-white bg-[#5F5BD7] rounded-lg w-full text-sm text-center grow">
+        <button className="text-white bg-sky-500 rounded-lg w-full text-sm text-center grow">
           Send recovery mail
         </button>
       </form>
@@ -31,4 +31,4 @@ const ForgotPassword_comp = () => {
   );
 };
 
-export default ForgotPassword_comp;
+export { ForgotPassword_comp };
