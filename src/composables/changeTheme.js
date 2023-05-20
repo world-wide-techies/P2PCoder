d0161219dea968b0b391068926e805135910439b
Ibimina hart <ibimina.c.hart@gmail.com>
2023-05-20 20:58:30 +0100

    docs: made review changes

--- src/composables/changeTheme.js
+++ src/composables/changeTheme.js
@@ -1,19 +1,11 @@
-import { useState } from "react";
-
 function useThemeChange() {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("appTheme") || "light"
-  );
-  const handleThemeChange = () => {
-    if (theme == "light") {
-      localStorage.setItem("appTheme", "dark");
-      setTheme(localStorage.getItem("appTheme"));
-    } else {
-      localStorage.setItem("appTheme", "light");
-      setTheme(localStorage.getItem("appTheme"));
-    }
-  };
-  return {theme, handleThemeChange}
+  if (theme == "light") {
+    localStorage.setItem("appTheme", "dark");
+    () => "dark";
+  } else {
+    localStorage.setItem("appTheme", "light");
+    () => "light";
+  }
 }
 
 export { useThemeChange };
