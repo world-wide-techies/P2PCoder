8994bc02d7a5be6e0578b919fc039314285af3d8
Taylor J <taylordjone@gmail.com>
2023-05-20 15:06:35 +0200

    updating spacing in form

--- src/components/signup_comp.js
+++ src/components/signup_comp.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function SignUpComponent() {
   return (
-    <form className="space-y-5">
+    <form className="space-y-5 p-6">
       <OnboardingHeader
         h1={"Create an account with us"}
         p={"Enjoy extra features when you create an account with us."}
@@ -25,70 +25,77 @@ function SignUpComponent() {
         <div className="border-b-2 border-gray-300 w-full relative flex justify-center"></div>
       </div>
 
-      <div className="flex justify-between space-x-3 w-full">
-        <div className="flex flex-col w-1/2 justify-start items-start">
-          <label>First Name</label>
-          <input
-            type="text"
-            name="firstname"
-            id="firstname"
-            placeholder="Enter First Name"
-            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
-          />
+      <div className="space-y-3">
+        <div className="w-full flex space-x-3">
+          <div className="flex flex-col w-1/2 justify-start items-start">
+            <label>First Name</label>
+            <input
+              type="text"
+              name="firstname"
+              id="firstname"
+              placeholder="Enter First Name"
+              className="border-[1px] border-gray p-3 rounded-lg bg-gray-100 w-full"
+            />
+          </div>
+
+          <div className="flex flex-col w-1/2 justify-start items-start">
+            <label>Last Name</label>
+            <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              placeholder="Enter Last Name"
+              className="border-[1px] border-gray p-3 rounded-lg bg-gray-100 w-full"
+            />
+          </div>
         </div>
 
-        <div className="flex flex-col w-1/2 justify-start items-start">
-          <label>Last Name</label>
-          <input
-            type="text"
-            name="lastname"
-            id="lastname"
-            placeholder="Enter Last Name"
-            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
-          />
-        </div>
-        <div className="flex flex-col w-1/2 justify-start items-start">
-          <label>Email Address</label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Enter Email Address"
-            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
-          />
+        <div className="w-full flex space-x-3">
+          <div className="flex flex-col w-1/2 justify-start items-start">
+            <label>Email Address</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Enter Email Address"
+              className="border-[1px] border-gray p-3 rounded-lg bg-gray-100 w-full"
+            />
+          </div>
+
+          <div className="flex flex-col w-1/2 justify-start items-start">
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter User Name"
+              className="border-[1px] border-gray p-3 rounded-lg bg-gray-100 w-full"
+            />
+          </div>
         </div>
 
-        <div className="flex flex-col w-1/2 justify-start items-start">
-          <label>Username</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Enter User Name"
-            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
-          />
-        </div>
+        <div className="w-full flex space-x-3">
+          <div className="flex flex-col w-1/2 justify-start items-start">
+            <label>Password*</label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              placeholder="Enter Password"
+              className="border-[1px] border-gray p-3 rounded-lg bg-gray-100 w-full"
+            />
+          </div>
 
-        <div className="flex flex-col w-1/2 justify-start items-start">
-          <label>Password*</label>
-          <input
-            type="text"
-            name="password"
-            id="password"
-            placeholder="Enter Password"
-            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
-          />
-        </div>
-
-        <div className="flex flex-col w-1/2 justify-start items-start">
-          <label>Confirm Password*</label>
-          <input
-            type="text"
-            name="lastname"
-            id="lastname"
-            placeholder="Re-enter Password"
-            className="border-[1px] border-gray p-3 rounded-lg bg-gray-100"
-          />
+          <div className="flex flex-col w-1/2 justify-start items-start">
+            <label>Confirm Password*</label>
+            <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              placeholder="Re-enter Password"
+              className="border-[1px] border-gray p-3 rounded-lg bg-gray-100 w-full"
+            />
+          </div>
         </div>
       </div>
 
