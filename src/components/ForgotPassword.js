5734f09d2e1eb96ff0da00c394ca4544d708f799
Oladipo <is.haqomotosho@gmail.com>
2023-05-20 15:35:35 +0100

    Created a function to reset Account using firebase

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,32 +1,36 @@
 import React from "react";
-import reset from "../assets/Icons/Light Mode Icon/reset.png";
-import Image from "next/image";
 const ForgotPassword = () => {
   return (
-    <div className="w-full flex flex-col justify-center items-start bg-white rounded-3xl shadow-lg">
-      <span>
-        <h3 className="text-blue-500 sm:text-lg font-bold ">Forgot Password</h3>
-        <Image src={reset} alt="reset" />
-      </span>
-      <p className="text-base leading-5 text-blue-950 font-normal">
-        Enter your email address to reset your password
-      </p>
-      <form>
-        <label
-          className="text-base leading-5 text-blue-950 font-normal"
-          htmlFor=""
-        >
-          Email Address
-        </label>
-        <input
-          type="text"
-          placeholder="Enter Email Address"
-          className="bg-neutral-50 rounded border-1 border-gray-400"
-        />
-        <button className="text-white bg-sky-500 rounded-lg w-full text-sm text-center grow">
-          Send recovery mail
-        </button>
-      </form>
+    <div className="flex flex-row justify-center items-center align-middle w-full">
+      <div className="w-full max-w-3xl flex flex-col justify-center items-start gap-y-4 bg-white rounded-3xl shadow-lg">
+        <div className="flex justify-center">
+          <h3 className="text-indigo-500 text-3xl font-bold ">
+            Forgot Password
+          </h3>
+          <button></button>
+        </div>
+        <p className="text-base leading-5 text-blue-950 font-normal">
+          Enter your email address to reset your password
+        </p>
+        <form className="w-full flex flex-col gap-y-4">
+          <label
+            className="text-base leading-5 text-blue-950 font-normal"
+            htmlFor="email"
+          >
+            Email Address
+          </label>
+          <input
+            id="email"
+            name="email_address"
+            type="email"
+            placeholder="Enter Email Address"
+            className="max-w-3xl bg-neutral-50 rounded border-2 border-gray-400"
+          />
+          <button className="w-full max-w-3xl text-white bg-indigo-500 rounded-lg text-sm text-center grow">
+            Send recovery mail
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
