e6aed048c7cc7d52dcc29f7819093c5371b7ca77
Ibhassan01 <olorunnisolaibrahim46@gmail.com>
2023-05-20 09:37:46 +0100

    verification successful component

--- src/components/VerificationSuccessful_comp.js
+++ src/components/VerificationSuccessful_comp.js
@@ -1,15 +1,17 @@
+import Link from "next/link";
+
 function VerificationSuccessful(){
     return (
         <div className="space-y-6">
             <div className="">
                 <div className="flex justify-between ">
                     <h1 className="text-blue-500 font-bold text-5xl">Verification Successful</h1>
-                    <h2>cancel</h2>
+                    <button>cancel</button>
                 </div>
                 <p className="my-6 text-2xl font-semibold">Account has been successfully verified with Carai</p>
-                <button className="px-6 py-3 bg-blue-600 my-7 text-white rounded-md font-bold border-[1px] w-full">
-                    Continue to account setup
-                </button>
+                <Link href="/login" className="hover:bg-blue-500 bg-violet-800 text-white text-center font-bold block w-full p-2 rounded-sm">
+                Log in
+                </Link>
             </div>
         </div>
     )
