a11bba32f8a9576a9f39136ca173b03d5dab100f
Chidera <kharunaking@gmail.com>
2023-05-20 05:53:23 -0400

    Update

--- src/components/VerificationOverlay.js
+++ src/components/VerificationOverlay.js
@@ -1,8 +1,3 @@
-<<<<<<< Updated upstream
-const VerificationOverlay = () => {
-  return (
-    <div className="absolute p-2">
-=======
 "use client";
 
 const VerificationOverlay = ({ email }) => {
@@ -15,33 +10,10 @@ const VerificationOverlay = ({ email }) => {
   };
   return (
     <div className="absolute w-full p-2">
->>>>>>> Stashed changes
       <div className="space-y-6 w-full bg-neutral-100 p-9 rounded-2xl ">
         <div className="text-blue-600 dark:text-white text-3xl font-extrabold">
           Account Verification
         </div>
-<<<<<<< Updated upstream
-        <p>
-          A verification link has been sent to{" "}
-          <span className="text-blue-600">augustine@example.com.</span> Please
-          check your inbox and click on the link to complete your registration.
-        </p>
-        <div>
-          <a href="#" className="text-blue-600 underline  font-bold">
-            Change Email
-          </a>
-        </div>
-        <div className="flex flex-col space-y-3">
-          <button className="bg-blue-600 hover:bg-blue-600 text-white px-6 py-3 rounded-md">
-            Open mail app
-          </button>
-        </div>
-        <p className="text-neutral-500">
-          Did not receive the mail? Check your spam
-          <span className="absolute  right-12">
-            Resend mail in <span className="text-black">00:45</span>
-          </span>
-=======
 
         <p>
           A verification link has been sent to{" "}
@@ -62,14 +34,9 @@ const VerificationOverlay = ({ email }) => {
         </div>
         <p className="text-neutral-500">
           Did not receive the mail? Check you spam
->>>>>>> Stashed changes
         </p>
       </div>
     </div>
   );
 };
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 export default VerificationOverlay;
