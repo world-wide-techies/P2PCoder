83b7634fd3b3845880c7e0ec26efb1ca04039814
Augustine Francis <enyiaugustinefrancis@gmail.com>
2023-05-21 21:31:18 +0100

    unreasonable commit

--- src/app/tests/page.js
+++ src/app/tests/page.js
@@ -1,7 +1,9 @@
 function TestPage() {
   return (
     <>
-      <div></div>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-6xl font-bold">Test Page {"==>"} Visit /tests</h1>
+      </div>
     </>
   );
 }
