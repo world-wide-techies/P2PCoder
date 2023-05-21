202292685dc6449c3c63a21f919f58695ad61ece
Augustine Francis <enyiaugustinefrancis@gmail.com>
2023-05-21 19:49:15 +0100

    update on main Editor Pages and navigators

--- src/components/navbar_components/editorNavbar_comp.js
+++ src/components/navbar_components/editorNavbar_comp.js
@@ -6,7 +6,7 @@ function EditorNavBar() {
   const [auth, setAuth] = useState(false);
   return (
     <main className="font-nohemi">
-      <div className="w-full  top-0 fixed p-3 bg-[#DCDCE5] dark:bg-[#2F2F3A]">
+      <div className="w-full  top-0 p-3 bg-[#DCDCE5] dark:bg-[#2F2F3A]">
         <div className="flex w-[95%] mx-auto justify-between items-center">
           <h1 className="dark:text-white text-[#5F5BD7] uppercase font-bold text-4xl">
             Carai
--- src/components/navbar_components/sidebar_components/sideBottomNavControl.js
+++ src/components/navbar_components/sidebar_components/sideBottomNavControl.js
@@ -10,7 +10,11 @@ function SideBottomNavControl() {
     <div className="flex flex-col justify-start items-center space-y-6 mt-12">
       {btnNav.map((e, i) => {
         return (
-          <button key={i} onClick={() => {}}>
+          <button
+            key={i}
+            onClick={() => {}}
+            className="hover:bg-gray-200 dark:hover:bg-gray-700 w-16 h-16 flex items-center justify-center hover:rounded-lg"
+          >
             <Image src={e} width={35} height={35} alt={`nav_btn_icon${i}`} />
           </button>
         );
