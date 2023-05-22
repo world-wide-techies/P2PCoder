8108d031f4b657087a0d6533df783aafe99ec5b6
Chidera <kharunaking@gmail.com>
2023-05-22 13:21:06 -0400

    Updated icon and dark theme

--- src/components/VerificationOverlay.js
+++ src/components/VerificationOverlay.js
@@ -1,22 +1,32 @@
 const VerificationOverlay = ({ email }) => {
   return (
-    <div className="absolute w-full p-2">
-      <div className="space-y-6 w-full bg-neutral-100 p-9 rounded-2xl ">
-        <div className="text-blue-600 dark:text-white text-3xl font-extrabold">
-          Account Verification
+    <div className="w-full p-3 bg-white">
+      <div className="space-y-6 w-full  bg-neutral-100 dark:bg-[#2F2F3A] p-9 rounded-2xl drop-shadow-md ">
+        <div className="text-indigo-500 dark:text-white text-3xl font-extrabold">
+          <h1>
+            Account Verification{" "}
+            <span className="absolute right-6 w-5 m-3">
+              <a href="#">
+                <img
+                  src="assets/onboardingIcons/close_black.png"
+                  alt="closeIcon"
+                ></img>
+              </a>
+            </span>
+          </h1>
         </div>
 
-        <p>
+        <p className="dark:text-white">
           A verification link has been sent to{" "}
-          <span className="text-blue-600">{email}.</span>
+          <span className="text-indigo-500">{email}.</span>
         </p>
-        <p>
+        <p className="dark:text-white">
           Please check your inbox and click on the link to complete your
           registration.
         </p>
         <div className="flex flex-col space-y-3">
           <a
-            className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-md text-center"
+            className="bg-indigo-500 hover:bg-indigo-400 text-white px-6 py-3 rounded-md text-center"
             href={`mailto:${email}`}
           >
             Open mail app
