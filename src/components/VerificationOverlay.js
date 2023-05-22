744330bbc00dc89b10ce53eea1355a5d14f7aabc
Chidera <kharunaking@gmail.com>
2023-05-22 14:12:46 -0400

    Updated bg color and href

--- src/components/VerificationOverlay.js
+++ src/components/VerificationOverlay.js
@@ -1,24 +1,26 @@
+import Link from "next/link";
+
 const VerificationOverlay = ({ email }) => {
   return (
     <div className="w-full p-3 bg-white">
       <div className="space-y-6 w-full  bg-neutral-100 dark:bg-[#2F2F3A] p-9 rounded-2xl drop-shadow-md ">
-        <div className="text-indigo-500 dark:text-white text-3xl font-extrabold">
+        <div className="text-blue-600 dark:text-white text-3xl font-extrabold">
           <h1>
             Account Verification{" "}
             <span className="absolute right-6 w-5 m-3">
-              <a href="#">
+              <link href="#">
                 <img
                   src="assets/onboardingIcons/close_black.png"
-                  alt="closeIcon"
+                  alt="close button icon"
                 ></img>
-              </a>
+              </link>
             </span>
           </h1>
         </div>
 
         <p className="dark:text-white">
           A verification link has been sent to{" "}
-          <span className="text-indigo-500">{email}.</span>
+          <span className="text-blue-600">{email}.</span>
         </p>
         <p className="dark:text-white">
           Please check your inbox and click on the link to complete your
@@ -26,7 +28,7 @@ const VerificationOverlay = ({ email }) => {
         </p>
         <div className="flex flex-col space-y-3">
           <a
-            className="bg-indigo-500 hover:bg-indigo-400 text-white px-6 py-3 rounded-md text-center"
+            className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-md text-center"
             href={`mailto:${email}`}
           >
             Open mail app
