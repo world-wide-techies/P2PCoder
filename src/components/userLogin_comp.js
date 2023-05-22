b99f439b767b633dadc8c1b38e9e82fc3cf3903e
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-05-23 00:10:48 +0400

    loginComponent aligned

--- src/components/userLogin_comp.js
+++ src/components/userLogin_comp.js
@@ -7,15 +7,15 @@ import gitHubIcon from "../../public/assets/onboardingIcons/github_black.png";
 
 function UserLoginComp() {
   return (
-    <form className="space-y-6 p-12">
+    <form className="space-y-6 p-10">
       <div className="space-y-3">
         <OnboardingHeader
           h1={"Welcome back"}
           p={"Enjoy extra features when you create an account with us."}
         />
 
-        <div className="flex justify-center spaxe-x-12">
-          <button className="bg-gray-200 flex justify-center items-center py-2 px-10 mx-3 rounded-md w-1/3 shadow-md">
+        <div className="flex justify-center space-x-5">
+          <button className="bg-gray-200 flex justify-center items-center p-3 rounded-md w-full shadow-md">
             <Image
               src={googleIcon}
               alt="google_icon"
@@ -23,10 +23,10 @@ function UserLoginComp() {
             ></Image>
             Create Account with Google
           </button>
-          <button className="bg-gray-200 flex justify-center items-center py-2 px-10 mx-3 rounded-md w-1/3 shadow-md">
+          <button className="bg-gray-200 flex justify-center items-center p-3 rounded-md w-full shadow-md">
             <Image
               src={gitHubIcon}
-              alt="google_icon"
+              alt="GitHub icon"
               className="w-7 h-7 mr-4"
             ></Image>
             Create Account with Github
@@ -34,10 +34,10 @@ function UserLoginComp() {
         </div>
       </div>
 
-      <div className="flex items-center">
-        <div className="border-b-2 border-gray-200 w-full flex justify-center"></div>
-        <p className="flex justify-center px-3">OR</p>
-        <div className="border-b-2 border-gray-200 w-full flex justify-center"></div>
+      <div className="flex items-center text-center">
+        <div className="border-b-2 border-gray-200 w-full relative flex justify-center"></div>
+        <p className="flex justify-center w-1/12">OR</p>
+        <div className="border-b-2 border-gray-200 w-full relative flex justify-center"></div>
       </div>
 
       <div className="space-y-8">
@@ -45,35 +45,33 @@ function UserLoginComp() {
           <label htmlFor="email address">Email Address</label>
           <input
             type="email"
-            className="w-full shadow-sm bg-gray-200 border-2 border-gray-300 rounded-sm outline-none p-2"
+            className="w-full shadow-sm bg-gray-200 border-2 border-gray-300 rounded-md p-3"
             placeholder="Enter Email Address"
           />
         </div>
 
         <div>
-          <label htmlFor="email address">
-            Password<sup>*</sup>
-          </label>
+          <label htmlFor="email address">Password*</label>
           <input
             type="email"
-            className="w-full shadow-sm bg-gray-200 border-2 border-gray-300 rounded-sm outline-none p-2"
-            placeholder="Write text here"
+            className="w-full shadow-sm bg-gray-200 border-2 border-gray-300 rounded-md p-3"
+            placeholder="Enter password"
           />
 
           <button className="float-right">Forgot password?</button>
         </div>
 
-        <div className="space-y-2">
+        <div className="space-y-3">
           <Link
             href="#"
-            className="bg-violet-800 text-white text-center font-bold block w-full p-2 rounded-sm"
+            className="bg-violet-800 text-white text-center font-bold block w-full p-3 rounded-md"
           >
             Log in
           </Link>
 
           <p className="text-center font-semibold">
             Don't have an account with us?
-            <Link href="#" className="text-violet-800">
+            <Link href="#" className="text-violet-800 mx-1.5">
               Create your account
             </Link>
           </p>
