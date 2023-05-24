293374bdc881422eadf0101f89b144bf58698941
Oladipo <is.haqomotosho@gmail.com>
2023-05-24 16:47:19 +0100

    added close icon to forgot password view component

--- src/app/tests/page.js
+++ src/app/tests/page.js
@@ -1,8 +1,11 @@
+import { ForgotPassword } from "@/components/ForgotPassword";
+
 function TestPage() {
   return (
     <>
       <div className="flex items-center justify-center h-screen">
-        <h1 className="text-6xl font-bold">Test Page {"==>"} Visit /tests</h1>
+        {/* <h1 className="text-6xl font-bold">Test Page {"==>"} Visit /tests</h1> */}
+        <ForgotPassword />
       </div>
     </>
   );
