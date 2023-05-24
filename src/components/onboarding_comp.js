6529e0cd3e97e96349d0af211ddb5c8f08524d2d
7malikk <7malikk@gmail.com>
2023-05-24 14:28:07 +0100

    implemented welcome component

--- src/components/onboarding_comp.js
+++ src/components/onboarding_comp.js
@@ -1,21 +1,59 @@
+import Image from 'next/image';
+import arrowIcon from '../../public/assets/onboardingIcons/arrow_light.png';
+
 function Onboarding() {
   return (
     <div className="space-y-6">
-      <div className="text-blue-500 dark:text-white text-xl font-extrabold">
+      <div className="text-blue-500 dark:text-white  text-5xl font-extrabold">
         Get Started With Carai
       </div>
-      <ul className="marker:text-blue-500 dark:marker:text-white dark:text-white list-disc px-6 space-y-6 font-bold text-lg">
-        <li>Share, Review & Improve your Code.</li>
-        <li>Connect with peers and code together.</li>
-        <li>Communicate with peers and code together.</li>
-        <li>Keep track of changes with code versionong.</li>
-        <li>Stop coding alone and start collaborating today.</li>
+      <ul className="dark:text-gray-400 space-y-6 font-bold ">
+        <li className="flex w-fit  dark:hover:text-white group items-center transition ease-in-out scale-100 translate-x-0 hover:translate-x-12 hover:scale-150  duration-500  ">
+          <Image
+            src={arrowIcon}
+            alt="arrowIconarrowIcon"
+            className=" hidden transition ease-in-out group-hover:translate-x-0  duration-500 group-hover:block"
+          />{' '}
+          <p> Share, Review & Improve your Code.</p>
+        </li>
+        <li className="flex w-fit dark:hover:text-white  group items-center transition ease-in-out scale-100 translate-x-0 hover:translate-x-12 hover:scale-150  duration-500  ">
+          <Image
+            src={arrowIcon}
+            alt="arrowIcon"
+            className=" hidden transition ease-in-out group-hover:translate-x-0  duration-500 group-hover:block"
+          />{' '}
+          <p> Connect with peers and code together.</p>
+        </li>
+        <li className="flex w-fit dark:hover:text-white group items-center transition ease-in-out scale-100 translate-x-0 hover:translate-x-12 hover:scale-150  duration-500  ">
+          <Image
+            src={arrowIcon}
+            alt="arrowIcon"
+            className=" hidden transition ease-in-out group-hover:translate-x-0  duration-500 group-hover:block"
+          />{' '}
+          <p>Communicate with peers and code together.</p>
+        </li>
+        <li className="flex w-fit dark:hover:text-white group items-center transition ease-in-out scale-100 translate-x-0 hover:translate-x-12 hover:scale-150  duration-500  ">
+          <Image
+            src={arrowIcon}
+            alt="arrowIcon"
+            className=" hidden transition ease-in-out group-hover:translate-x-0  duration-500 group-hover:block"
+          />{' '}
+          <p> Keep track of changes with code versioning.</p>
+        </li>
+        <li className="flex w-fit dark:hover:text-white group items-center transition ease-in-out scale-100 translate-x-0 hover:translate-x-14 hover:scale-150  duration-500  ">
+          <Image
+            src={arrowIcon}
+            alt="arrowIcon"
+            className=" hidden transition ease-in-out group-hover:translate-x-0  duration-500 group-hover:block"
+          />{' '}
+          <p> Stop coding alone and start collaborating today.</p>
+        </li>
       </ul>
-      <div className="flex flex-col space-y-3">
+      <div className="flex flex-col  space-y-3 w-1/2">
         <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-md">
           Create an Account
         </button>
-        <button className="px-6 py-3 hover:bg-blue-600 hover:text-white rounded-md border-[1px]">
+        <button className="px-6 py-3 hover:bg-blue-600 hover:text-white dark:bg-white dark:hover:bg-gray-200 dark:hover:text-blue-500 dark:border-0 rounded-md border">
           Log In
         </button>
       </div>
