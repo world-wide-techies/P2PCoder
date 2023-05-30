03e30d642ba45b2564fdf37d70fbb44f15a62766
Ibhassan01 <olorunnisolaibrahim46@gmail.com>
2023-05-30 22:35:29 +0100

    added bottomNavBarIcons

--- src/app/tests/page.js
+++ src/app/tests/page.js
@@ -1,3 +1,11 @@
-import HtmlScreen from "@/components/HtmlScreen_comp";
+function TestPage() {
+    return (
+        <>
+            <div className="flex items-center justify-center h-screen">
+                <h1 className="text-6xl font-bold">Test Page {"==>"} Visit /tests</h1>
+            </div>
+        </>
+    );
+}
 
-export default HtmlScreen;
+export default TestPage;
