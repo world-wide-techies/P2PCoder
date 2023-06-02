e70288c3568aac3d27e74098e77e55340561a3fa
Chidera <kharunaking@gmail.com>
2023-06-02 13:51:14 -0400

    Update on session management

--- src/composables/sessionManagement.js
+++ src/composables/sessionManagement.js
@@ -1,66 +1,67 @@
 import { generatePeerIdCharacter } from "./peerIdGenerator";
 
-// Function to generate peerId and time
-function generatePeerIdAndTime() {
-  const peerId = generatePeerIdCharacter();
-
-  const currentTime = new Date();
-  const minutes = currentTime.getMinutes();
-  const seconds = currentTime.getSeconds();
-
-  const formattedTime = `${minutes}:${seconds}`;
-
-  return { peerId, generatedTime: formattedTime };
-}
-// Store session data in the local storage
+// In event order =>
 function storeSessionData(users) {
   localStorage.setItem("users", JSON.stringify(users));
 }
 
-// Retrieve session data from the local storage
 function getSessionData() {
   const users = JSON.parse(localStorage.getItem("users"));
-
   return users;
 }
-// Timer function to terminate session
-function startTimer() {
+
+function startTimer(joinTime) {
   const currentTime = new Date();
   const currentMinutes = currentTime.getMinutes();
   const currentSeconds = currentTime.getSeconds();
 
   const [joinMinutes, joinSeconds] = joinTime.split(":").map(Number);
 
-  // Calculate the time difference between the current time and the join time
   const timeDifference =
     currentMinutes * 60 + currentSeconds - (joinMinutes * 60 + joinSeconds);
 
   if (timeDifference >= 59 * 60 + 59) {
     localStorage.removeItem("users");
+    return;
   } else {
     setTimeout(() => startTimer(joinTime), 1000);
   }
 }
-// Retrieve or initialize session data
-let sessionData = getSessionData();
-if (!sessionData || sessionData.length !== 2) {
-  const user1 = generatePeerIdAndTime();
-  const user2 = generatePeerIdAndTime();
 
-  // Store the session data in the local storage
-  sessionData = [user1, user2];
+const peerId = generatePeerIdCharacter();
+
+let sessionData = getSessionData();
+if (!sessionData) {
+  sessionData = [];
   storeSessionData(sessionData);
 }
 
-// Determine the user's index based on their peer ID
-const userIndex = sessionData.findIndex(
-  (user) => user.peerId === generatePeerIdCharacter()
-);
-if (userIndex !== -1) {
-  const currentUser = sessionData[userIndex];
+let currentUser;
+const userIndex = sessionData.findIndex((user) => user.peerId === peerId);
 
-  // Start the timer using the join time of the current user
-  startTimer(currentUser.generatedTime);
+if (userIndex !== -1 && sessionData.length < 2) {
+  currentUser = {
+    peerId,
+    joinTime: new Date().toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
+  };
+
+  sessionData.push(currentUser);
+  storeSessionData(sessionData);
+} else if (userIndex !== -1) {
+  currentUser = sessionData[userIndex];
+
+  const storedJoinTime = getSessionData()[userIndex].joinTime;
+
+  startTimer(storedJoinTime);
+} else {
+  throw new Error("Session is at maximum users");
 }
 
-export { generatePeerIdAndTime, storeSessionData, getSessionData };
+window.onbeforeunload = () => {
+  storeSessionData(sessionData);
+};
+
+export { storeSessionData, getSessionData };
