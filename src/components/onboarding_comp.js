96df422af8a5b3022f76592a94c78b06262e1bcb
Chidera <kharunaking@gmail.com>
2023-06-05 14:45:29 -0400

    Session onboarding component

--- src/components/onboarding_comp.js
+++ src/components/onboarding_comp.js
@@ -1,8 +1,8 @@
-import Image from 'next/image';
-import arrowIconDark from '../../public/assets/onboardingIcons/arrow_dark.png';
-import arrowIconLight from '../../public/assets/onboardingIcons/arrow_light.png';
-import Link from 'next/link';
-import { useTheme } from 'next-themes';
+import Image from "next/image";
+import arrowIconDark from "../../public/assets/onboardingIcons/arrow_dark.png";
+import arrowIconLight from "../../public/assets/onboardingIcons/arrow_light.png";
+import Link from "next/link";
+import { useTheme } from "next-themes";
 
 function Onboarding() {
   const { theme, setTheme } = useTheme();
@@ -16,7 +16,7 @@ function Onboarding() {
           <Image
             width={26}
             height={26}
-            src={theme === 'dark' ? arrowIconLight : arrowIconDark}
+            src={theme === "dark" ? arrowIconLight : arrowIconDark}
             alt="arrowIcon"
             className=" hidden transition ease-in-out group-hover:translate-x-0  duration-500 group-hover:block"
           />
@@ -26,52 +26,54 @@ function Onboarding() {
           <Image
             width={26}
             height={26}
-            src={theme === 'dark' ? arrowIconLight : arrowIconDark}
+            src={theme === "dark" ? arrowIconLight : arrowIconDark}
             alt="arrowIcon"
             className=" hidden transition ease-in-out group-hover:translate-x-0  duration-500 group-hover:block"
-          />{' '}
+          />{" "}
           <p> Connect with peers and code together.</p>
         </li>
         <li className="flex w-fit dark:hover:text-white group items-center transition ease-in-out scale-100 translate-x-0 hover:translate-x-12 hover:scale-150  duration-500  ">
           <Image
             width={26}
             height={26}
-            src={theme === 'dark' ? arrowIconLight : arrowIconDark}
+            src={theme === "dark" ? arrowIconLight : arrowIconDark}
             alt="arrowIcon"
             className=" hidden transition ease-in-out group-hover:translate-x-0  duration-500 group-hover:block"
-          />{' '}
+          />{" "}
           <p>Communicate with peers and code together.</p>
         </li>
         <li className="flex w-fit dark:hover:text-white group items-center transition ease-in-out scale-100 translate-x-0 hover:translate-x-12 hover:scale-150  duration-500  ">
           <Image
             width={26}
             height={26}
-            src={theme === 'dark' ? arrowIconLight : arrowIconDark}
+            src={theme === "dark" ? arrowIconLight : arrowIconDark}
             alt="arrowIcon"
             className=" hidden transition ease-in-out group-hover:translate-x-0  duration-500 group-hover:block"
-          />{' '}
+          />{" "}
           <p> Keep track of changes with code versioning.</p>
         </li>
         <li className="flex w-fit dark:hover:text-white group items-center transition ease-in-out scale-100 translate-x-0 hover:translate-x-14 hover:scale-150  duration-500  ">
           <Image
             width={26}
             height={26}
-            src={theme === 'dark' ? arrowIconLight : arrowIconDark}
+            src={theme === "dark" ? arrowIconLight : arrowIconDark}
             alt="arrowIcon"
             className=" hidden transition ease-in-out group-hover:translate-x-0  duration-500 group-hover:block"
-          />{' '}
+          />{" "}
           <p> Stop coding alone and start collaborating today.</p>
         </li>
       </ul>
       <div className="flex flex-col  space-y-3 w-1/3">
         <Link
-          href={'/?view=signup'}
-          className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-md flex justify-center">
+          href={"/?view=signup"}
+          className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-md flex justify-center"
+        >
           Create an Account
         </Link>
         <Link
-          href={'/?view=login'}
-          className="px-6 py-3 hover:bg-blue-600 hover:text-white dark:text-black dark:bg-white dark:hover:bg-gray-200 dark:hover:text-blue-500 dark:border-0 rounded-md border flex justify-center">
+          href={"/?view=login"}
+          className="px-6 py-3 hover:bg-blue-600 hover:text-white dark:text-black dark:bg-white dark:hover:bg-gray-200 dark:hover:text-blue-500 dark:border-0 rounded-md border flex justify-center"
+        >
           Log In
         </Link>
       </div>
