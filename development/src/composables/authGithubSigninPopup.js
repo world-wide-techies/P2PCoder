db512a43c07a7581b2ea93c843d02c59c4c3b547
7malikk <7malikk@gmail.com>
2023-06-06 23:55:21 +0100

    refactor

--- development/src/composables/authGithubSigninPopup.js
+++ development/src/composables/authGithubSigninPopup.js
@@ -1,22 +1,22 @@
-// import { signInWithPopup, GithubAuthProvider } from 'firebase/auth';
-// import { appAuth } from './firebaseConfig/config';
+import { signInWithPopup, GithubAuthProvider } from 'firebase/auth';
+import { appAuth } from './firebaseConfig/config';
 
-// const provider = new GithubAuthProvider();
+const provider = new GithubAuthProvider();
 
-// async function signInWithGithub() {
-//   try {
-//     const res = await signInWithPopup(appAuth, provider);
-//     if (!res) {
-//       throw new Error("Couldn't complete signup");
-//     }
-//     const user = res.user;
-//     return user;
-//   } catch (error) {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     const credential = GithubAuthProvider.credentialFromError(error);
-//     throw new Error(errorMessage);
-//   }
-// }
+async function signInWithGithub() {
+  try {
+    const res = await signInWithPopup(appAuth, provider);
+    if (!res) {
+      throw new Error("Couldn't complete signup");
+    }
+    const user = res.user;
+    return user;
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const credential = GithubAuthProvider.credentialFromError(error);
+    throw new Error(errorMessage);
+  }
+}
 
-// export { signInWithGithub };
+export { signInWithGithub };
--- development/src/composables/sendAccountResetEmail.js
+++ development/src/composables/sendAccountResetEmail.js
@@ -1,14 +1,14 @@
-// import { sendPasswordResetEmail } from 'firebase/auth';
-// import { appAuth } from './firebaseConfig/config';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { appAuth } from './firebaseConfig/config';
 
-// async function sendAccountResetEmail() {
-//   try {
-//     await sendPasswordResetEmail(appAuth, email);
-//     return true;
-//   } catch (error) {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     return errorMessage;
-//   }
-// }
-// export { sendAccountResetEmail };
+async function sendAccountResetEmail() {
+  try {
+    await sendPasswordResetEmail(appAuth, email);
+    return true;
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    return errorMessage;
+  }
+}
+export { sendAccountResetEmail };
