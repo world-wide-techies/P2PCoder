e524edceb91b0a1957cc74e1dede014ecba63cdf
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-06-10 23:43:30 +0400

    Users are now  able to recover account from form

--- development/src/components/userLogin_comp.js
+++ development/src/components/userLogin_comp.js
@@ -16,6 +16,9 @@ import {
 import { signInWithGithub } from "@/composables/authGithubSigninPopup";
 import { PasswordToggle } from "./passwordToggleFunction";
 import { signInWithGoogle } from "@/composables/authGoogleSigninPoppup";
+import { ForgotPassword } from "./ForgotPassword";
+import closeWhite from ".././../public/assets/forgotPasswordForm/close_white.png";
+import closeBlack from ".././../public/assets/onboardingIcons/close_black.png";
 
 function UserLoginComp() {
   const { theme, setTheme } = useTheme();
@@ -23,6 +26,12 @@ function UserLoginComp() {
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [forgotPasswordModal, setForgotPasswordModal] = useState(false);
+
+
+  const forgotPassword = () => {
+    setForgotPasswordModal(true);
+  };
 
   const emailChange = (e) => {
     setEmailAddress(e.target.value);
@@ -65,99 +74,126 @@ function UserLoginComp() {
   };
 
   return (
-    <form
-      className="space-y-6 p-10 bg-white dark:bg-[#1E1E2A] dark:text-white"
-      onSubmit={loginUser}
-    >
-      <div className="space-y-3">
-        <OnboardingHeader
-          h1={"Welcome back"}
-          p={"Enjoy extra features when you create an account with us."}
-        />
-
-        <div className="flex flex-row justify-between gap-3">
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              signInWithGoogle();
-            }}
-            className="flex flex-row flex-nowrap justify-center gap-2 bg-gray-200 dark:bg-[#363647] items-center p-3 rounded-md w-full shadow-md"
+      <div>
+        {!forgotPasswordModal ? (
+          <form
+            className="space-y-6 p-10 bg-white dark:bg-[#1E1E2A] dark:text-white w-auto min-w-[600px] font-nohemi rounded-[24px]"
+            onSubmit={loginUser}
           >
-            <Image src={googleIcon} alt="google_icon" className="w-6 h-auto" />
-            <p className="text-[10px]">Create Account with Google</p>
-          </button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              signInWithGithub();
-            }}
-            className="flex flex-row flex-nowrap justify-center gap-2 bg-gray-200 dark:bg-[#363647] items-center p-3 rounded-md w-full shadow-md"
-          >
-            <Image
-              src={theme === "dark" ? github_darkMode : github_lightMode}
-              alt="google_icon"
-              className="w-6 h-auto"
-            />
-            <p className="text-[10px]">Create Account with Github</p>
-          </button>
-        </div>
+            <div className="space-y-3">
+              <div className="flex justify-between">
+                <OnboardingHeader
+                  h1={"Welcome back"}
+                  p={"Enjoy extra features when you create an account with us."}
+                />
+
+                <Image
+                  src={theme === "dark" ? closeWhite : closeBlack}
+                  alt="closeBtn"
+                  width={24}
+                  height={24}
+                  className="w-6 h-6 ml-8 mb-[20px]"
+                />
+              </div>
+
+              <div className="flex flex-row justify-between gap-3">
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signInWithGoogle();
+                  }}
+                  className="flex flex-row flex-nowrap justify-center gap-2 bg-[#DCDCE5] dark:bg-[#363647] items-center p-3 rounded-md w-full shadow-md"
+                >
+                  <Image
+                    src={googleIcon}
+                    alt="google_icon"
+                    className="w-6 h-auto"
+                  />
+                  <p className="text-[14px]">Create Account with Google</p>
+                </button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signInWithGithub();
+                  }}
+                  className="flex flex-row flex-nowrap justify-center gap-2 bg-[#DCDCE5] dark:bg-[#363647] items-center px-3 rounded-md w-full shadow-md"
+                >
+                  <Image
+                    src={theme === "dark" ? github_darkMode : github_lightMode}
+                    alt="google_icon"
+                    className="w-6 h-auto"
+                  />
+                  <p className="text-[14px]">Create Account with Github</p>
+                </button>
+              </div>
+            </div>
+
+            <div className="flex items-center text-center">
+              <div className="border-b-2 border-gray-200 w-full relative flex justify-center"></div>
+              <p className="flex justify-center w-1/6">OR</p>
+              <div className="border-b-2 border-gray-200 w-full relative flex justify-center"></div>
+            </div>
+
+            <div className="space-y-8">
+              <div>
+                <label htmlFor="email">Email Address</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="dark:bg-[#363647] border-[1px] p-3 rounded-lg bg-gray-100 w-full"
+                  placeholder="Enter Email Address"
+                  onChange={emailChange}
+                  value={emailAddress}
+                />
+                {emailError && (
+                  <p className="text-sm text-red-500">{emailError}</p>
+                )}
+              </div>
+
+              <div>
+                <label htmlFor="password">Password</label>
+                <PasswordToggle
+                  inputId="password"
+                  placeholder="Enter password"
+                  handleInputChange={passwordChange}
+                  inputValue={password}
+                />
+                {passwordError && (
+                  <p className="text-sm text-red-500">{passwordError}</p>
+                )}
+                <button
+                  className="float-right font-bold text-[#5F5BD7]"
+                  onClick={forgotPassword}
+                >
+                  Forgot password?
+                </button>
+              </div>
+
+              <div className="space-y-3">
+                <button
+                  type="submit"
+                  className="bg-[#5F5BD7] text-white text-center font-bold block w-full p-3 rounded-md"
+                >
+                  Log in
+                </button>
+
+                <p className="text-center">
+                  {"Don't have an account with us?"}
+                  <Link
+                    href="/?view=signup"
+                    className="text-violet-800 mx-1.5 dark:text-white font-bold"
+                  >
+                    Create your account
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </form>
+        ) : (
+          <ForgotPassword />
+        )}
       </div>
-
-      <div className="flex items-center text-center">
-        <div className="border-b-2 border-gray-200 w-full relative flex justify-center"></div>
-        <p className="flex justify-center w-1/6">OR</p>
-        <div className="border-b-2 border-gray-200 w-full relative flex justify-center"></div>
-      </div>
-
-      <div className="space-y-8">
-        <div>
-          <label htmlFor="email">Email Address</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="dark:bg-[#363647] border-[1px] p-3 rounded-lg bg-gray-100 w-full"
-            placeholder="Enter Email Address"
-            onChange={emailChange}
-            value={emailAddress}
-          />
-          {emailError && <p className="text-sm text-red-500">{emailError}</p>}
-        </div>
-
-        <div>
-          <label htmlFor="password">Password</label>
-          <PasswordToggle
-            inputId="password"
-            placeholder="Enter password"
-            handleInputChange={passwordChange}
-            inputValue={password}
-          />
-          {passwordError && (
-            <p className="text-sm text-red-500">{passwordError}</p>
-          )}
-          <button className="float-right">Forgot password?</button>
-        </div>
-
-        <div className="space-y-3">
-          <button
-            type="submit"
-            className="bg-violet-800 text-white text-center font-bold block w-full p-3 rounded-md"
-          >
-            Log in
-          </button>
-
-          <p className="text-center">
-            {"Don't have an account with us?"}
-            <Link
-              href="/?view=signup"
-              className="text-violet-800 mx-1.5 dark:text-white font-semibold"
-            >
-              Create your account
-            </Link>
-          </p>
-        </div>
-      </div>
-    </form>
   );
 }
 export default UserLoginComp;
