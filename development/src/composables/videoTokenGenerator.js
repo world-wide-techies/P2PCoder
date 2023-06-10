8c6d5043d8b3aaa57b4864a5a01322342c11e74e
Ibhassan01 <olorunnisolaibrahim46@gmail.com>
2023-06-10 22:00:21 +0100

    function for generating access token

---
+++ development/src/composables/videoTokenGenerator.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const AccessToken = require('twilio').jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+
+function tokenGenerator(identity, room) {
+    const token = new AccessToken(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_API_KEY,
+        process.env.TWILIO_API_SECRET
+    );
+
+    token.identity = identity;
+
+    const grant = new VideoGrant();
+    grant.room = room;
+    token.addGrant(grant);
+
+    const jwtToken = token.toJwt();
+    console.log(jwtToken);
+
+    return jwtToken;
+}
+
+module.exports = tokenGenerator;
