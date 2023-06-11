502cdbd88c57a594ce25dbb568c9ad41083151c6
Taylor J <taylordjone@gmail.com>
2023-06-11 22:34:02 +0200

    adding in form validation

--- development/src/composables/signupFormValidation.js
+++ development/src/composables/signupFormValidation.js
@@ -2,44 +2,49 @@ function signupFormValidation(user) {
   const errors = {};
 
   const regxName = /^[A-Za-z\-]{2,50}$/;
-  if (!user.firstname) {
+  const trimmedFirstName = user.firstname.trim();
+  if (!trimmedFirstName) {
     errors.firstname = "Please enter your first name";
-  } else if (user.firstname.length < 2 || user.firstname.length > 50) {
+  } else if (trimmedFirstName < 2 || trimmedFirstName > 50) {
     errors.firstname = "Name must be between 2 and 50 characters";
-  } else if (!user.firstname.match(regxName)) {
+  } else if (!regxName.test(trimmedFirstName)) {
     errors.firstname = "Please enter a valid name";
   }
 
   const regxLastName = /^[A-Za-z\-]{2,50}$/;
-  if (!user.lastname) {
+  const trimmedLastName = user.lastname.trim();
+  if (!trimmedLastName) {
     errors.lastname = "Please enter your last name";
-  } else if (user.lastname.length < 2 || user.lastname.length > 50) {
+  } else if (trimmedLastName < 2 || trimmedLastName > 50) {
     errors.lastname = "Name must be between 2 and 50 characters";
-  } else if (!user.lastname.match(regxLastName)) {
+  } else if (!regxLastName.test(trimmedLastName)) {
     errors.lastname = "Please enter a valid name";
   }
 
   const regxEmail = /^([a-zA-Z0-9\.]+)@([a-zA-Z]+)\.([a-z]+)(\.[a-z]+)?$/;
-  if (!user.email) {
+  const trimmedEmail = user.email.trim();
+  if (!trimmedEmail) {
     errors.email = "Please enter your email address";
-  } else if (!user.email.match(regxEmail)) {
+  } else if (!regxEmail.test(trimmedEmail)) {
     errors.email = "Please enter a valid email address";
   }
 
   const regxUsername = /^[a-zA-Z0-9_]{3,20}$/;
-  if (!user.username) {
+  const trimmedUserName = user.username.trim();
+  if (!trimmedUserName) {
     errors.username = "Please enter your username";
-  } else if (!user.username.match(regxUsername)) {
+  } else if (!regxUsername.test(trimmedUserName)) {
     errors.username = "Please enter a valid username";
   }
 
   const regxPassword =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-  if (!user.password) {
+  const trimmedPassword = user.password.trim();
+  if (!trimmedPassword) {
     errors.password = "Please enter a password";
-  } else if (user.password.length < 8 || user.password.length > 20) {
+  } else if (trimmedPassword.length < 8 || trimmedPassword.length > 20) {
     errors.password = "Please enter a password between 8 and 15 characters";
-  } else if (!user.password.match(regxPassword)) {
+  } else if (!regxPassword.test(trimmedPassword)) {
     errors.password =
       "Password must contain one digit, one lowercase letter, one uppercase letter, and one special character";
   }
