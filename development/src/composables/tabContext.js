ee982f1d18ffa633de05f6308c8dd9168deb241b
DeborahIhesiaba <deborahihesiaba@gmail.com>
2023-06-13 18:29:41 +0200

    Quick fix

--- development/src/composables/tabContext.js
+++ development/src/composables/tabContext.js
@@ -1,6 +1,4 @@
-import ErrorModal from "@/components/errorModal_comp";
 import { createContext, useContext, useEffect, useState } from "react";
-import { toast } from "react-toastify";
 
 export const TabContext = createContext();
 
