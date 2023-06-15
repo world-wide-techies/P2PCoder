93f3413565c93fe68b8aafab2cecde814a2fa494
Augustine Francis <enyiaugustinefrancis@gmail.com>
2023-06-15 23:33:57 +0100

    update on next.config

--- development/next.config.js
+++ development/next.config.js
@@ -1,10 +1,10 @@
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // webpack: (config, { dev, isServer }) => {
-  //   config.plugins.push(new MonacoWebpackPlugin());
-  //   return config;
-  // },
+  webpack: (config, { dev, isServer }) => {
+    config.plugins.push(new MonacoWebpackPlugin());
+    return config;
+  },
 };
 
 module.exports = nextConfig;
