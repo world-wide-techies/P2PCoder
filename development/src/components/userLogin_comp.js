8c90e52fff4097b9720e3260f2ea6c3f9fc408a1
Oyinbrakemigrace <graceofubu04@gmail.com>
2023-06-16 21:02:30 +0400

    users account recovery

--- development/src/components/userLogin_comp.js
+++ development/src/components/userLogin_comp.js
@@ -12,21 +12,16 @@ import {
   emailValidator,
   passwordValidator,
 } from "@/composables/emailPasswordValidator";
-import { signInWithGithub } from "@/composables/authGithubSigninPopup";
-import { PasswordToggle } from "./passwordToggleFunction";
-import { signInWithGoogle } from "@/composables/authGoogleSigninPoppup";
+
 import closeWhite from ".././../public/assets/forgotPasswordForm/close_white.png";
 import closeBlack from ".././../public/assets/onboardingIcons/close_black.png";
 import { useRouter } from "next/navigation";
 
 import { useGithubSignin } from "@/composables/authGithubSigninPopup";
 import { PasswordToggle } from "./passwordToggleFunction";
-import { signInWithGoogle } from "@/composables/authGoogleSigninPoppup";
 import closeIcon from "../../public/assets/onboardingIcons/close_light.png";
 import closeDark from "../../public/assets/onboardingIcons/closecircledark.png";
-import {
-  useGoogleSignin,
-} from "@/composables/authGoogleSigninPoppup";
+import { useGoogleSignin } from "@/composables/authGoogleSigninPoppup";
 import ErrorModal from "./errorModal_comp";
 
 function UserLoginComp() {
@@ -54,12 +49,12 @@ function UserLoginComp() {
 
   const emailChange = (e) => {
     setEmailAddress(e.target.value);
-    setEmailError(false)
+    setEmailError(false);
   };
 
   const passwordChange = (e) => {
     setPassword(e.target.value);
-    setPasswordError(false)
+    setPasswordError(false);
   };
 
   const handleCloseLogin = () => {
@@ -104,84 +99,7 @@ function UserLoginComp() {
       className="space-y-6 p-10 bg-white dark:bg-[#1E1E2A] dark:text-white w-auto min-w-[600px] font-nohemi rounded-[24px]"
       onSubmit={loginUser}
     >
-      <div className="space-y-3">
-        <div className="flex justify-between">
-          <OnboardingHeader
-            h1={"Welcome back"}
-            p={"Enjoy extra features when you create an account with us."}
-          />
-          <Image
-            src={theme === "dark" ? closeIcon : closeDark}
-            alt="close icon"
-            className="w-6 h-6 mr-4 mt-2"
-          />
-        </div>
-        <div className="flex flex-row justify-between gap-3">
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              signinWithGoogle();
-            }}
-            className="flex flex-row flex-nowrap justify-center gap-2 bg-gray-200 dark:bg-[#363647] items-center p-3 rounded-md w-full shadow-md"
-          >
-            <Image src={googleIcon} alt="google_icon" className="w-6 h-auto" />
-            <p className="text-[10px]">Create Account with Google</p>
-          </button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              signinWithGithub();
-            }}
-            className="flex flex-row flex-nowrap justify-center gap-2 bg-gray-200 dark:bg-[#363647] items-center p-3 rounded-md w-full shadow-md"
-          >
-            <Image
-              src={theme === "dark" ? github_darkMode : github_lightMode}
-              alt="google_icon"
-              className="w-6 h-auto"
-            />
-            <p className="text-[10px]">Create Account with Github</p>
-          </button>
-        </div>
-      </div>
-
-      <div className="flex items-center text-center">
-        <div className="border-b-2 border-gray-200 w-full relative flex justify-center"></div>
-        <p className="flex justify-center w-1/6">OR</p>
-        <div className="border-b-2 border-gray-200 w-full relative flex justify-center"></div>
-      </div>
-
       <div className="space-y-8">
-        <div>
-          <label htmlFor="email">Email Address</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="dark:bg-[#363647] border-[1px] p-3 rounded-lg bg-gray-100 w-full"
-            placeholder="Enter Email Address"
-            onChange={emailChange}
-            value={emailAddress}
-          />
-          {emailError && <p className="text-sm text-red-500">{emailError}</p>}
-        </div>
-
-        <div>
-          <label htmlFor="password">Password</label>
-          <PasswordToggle
-            inputId="password"
-            placeholder="Enter password"
-            handleInputChange={passwordChange}
-            inputValue={password}
-            customClass={`border ${
-              passwordError ? "border-[#ec6d6a]" : "border-none"
-            } p-3 rounded-lg dark:bg-[#363647] bg-gray-100 w-full`}
-          />
-          {passwordError && (
-            <p className="text-sm text-red-500">{passwordError}</p>
-          )}
-          <button className="float-right">Forgot password?</button>
-        </div>
-
         <div className="space-y-3">
           <div className="flex justify-between">
             <OnboardingHeader
@@ -203,7 +121,7 @@ function UserLoginComp() {
             <button
               onClick={(e) => {
                 e.preventDefault();
-                signInWithGoogle();
+                signinWithGoogle();
               }}
               className="flex flex-row flex-nowrap justify-center gap-2 bg-[#DCDCE5] dark:bg-[#363647] items-center p-3 rounded-md w-full"
             >
@@ -217,7 +135,7 @@ function UserLoginComp() {
             <button
               onClick={(e) => {
                 e.preventDefault();
-                signInWithGithub();
+                signinWithGithub();
               }}
               className="flex flex-row flex-nowrap justify-center gap-2 bg-[#DCDCE5] dark:bg-[#363647] items-center px-3 rounded-md w-full"
             >
@@ -298,13 +216,12 @@ function UserLoginComp() {
           </div>
         </div>
         <ErrorModal
-        errorMessage={errorMessage}
-        style={"fixed  top-0 right-0 mr-2 "}
-        onClose={() => handleClose()}
-      />
+          errorMessage={errorMessage}
+          style={"fixed  top-0 right-0 mr-2 "}
+          onClose={() => handleClose()}
+        />
       </div>
-      </form>
-      
+    </form>
   );
 }
 export default UserLoginComp;
