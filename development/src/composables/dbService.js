baa0c388eaf62c8fd4ac9057ffa8f87ae10df234
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-06-17 19:44:00 +0100

    Updated

--- development/src/composables/dbService.js
+++ development/src/composables/dbService.js
@@ -1,9 +1,12 @@
-import { appFirestore, appAuth, } from "./firebaseConfig/config";
+import React from "react";
+
+import { appFirestore, appAuth } from "./firebaseConfig/config";
 import { setDoc, doc, updateDoc, getDoc } from "firebase/firestore";
 
-async function addSession(userSessionData) {
-  const { sessionName, peerId, language, userName, peerName } = userSessionData;
-  if (!sessionName || !peerId || !language || !userName || !peerName) {
+async function createSession(sessionData) {
+  console.log(sessionData);
+  const { sessionName, activeLanguage, peerSessionId } = sessionData;
+  if (!sessionName || !activeLanguage || !peerSessionId) {
     throw new Error("Kindly provide all fields");
   }
 
@@ -13,16 +16,14 @@ async function addSession(userSessionData) {
   }
 
   const coders = doc(appFirestore, `CODERS/${user.uid}`);
-  const session = doc(coders, `SESSION/${peerId}`);
+  const session = doc(coders, `SESSION/${peerSessionId}`);
 
   try {
     const sessionData = {
       sessionName: sessionName,
-      peerName: peerName,
-      peerId: peerId,
-      language: language,
+      peerId: peerSessionId,
+      language: activeLanguage,
       createdAt: new Date(),
-      userName: userName,
     };
     const docRef = await setDoc(session, sessionData, { merge: true });
     return "session added";
@@ -78,16 +79,8 @@ async function addCollabCodeEditor(codeEditorData) {
 }
 
 async function updateSession(updatedSessionData) {
-  const { editorCode, sessionName, peerId, language, userName, peerName } =
-    updatedSessionData;
-  if (
-    !editorCode ||
-    !sessionName ||
-    !peerId ||
-    !language ||
-    !userName ||
-    !peerName
-  ) {
+  const { editorCode, peerSessionId, activeLanguage } = updatedSessionData;
+  if (!editorCode || !peerSessionId || !activeLanguage) {
     throw new Error("Kindly provide all fields");
   }
 
@@ -97,17 +90,15 @@ async function updateSession(updatedSessionData) {
   }
 
   const coders = doc(appFirestore, `CODERS/${user.uid}`);
-  const session = doc(coders, `SESSION/${peerId}`);
+  const session = doc(coders, `SESSION/${peerSessionId}`);
 
   try {
     const sessionData = {
       codes: editorCode,
       sessionName: sessionName,
-      peerName: peerName,
-      peerId: peerId,
+      peerId: peerSessionId,
       language: language,
       endedAt: new Date(),
-      userName: userName,
     };
     const docRef = await updateDoc(session, sessionData);
     return "session updated!";
@@ -116,4 +107,4 @@ async function updateSession(updatedSessionData) {
   }
 }
 
-export { addCollabCodeEditor, addSession, getUserDetails, updateSession };
+export { addCollabCodeEditor, createSession, getUserDetails, updateSession };
