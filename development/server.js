988c24896801edf335456207d810663074487edf
Augustine Francis <enyiaugustinefrancis@gmail.com>
2023-06-21 02:52:24 +0100

    update on code_editor_servers.

---
+++ development/server.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`Client joined room: ${room}`);
+  });
+
+  socket.on("text-update", (data) => {
+    // Broadcast the received update to all connected clients
+    io.to(data.room).emit("text-update", data.newValue);
+    console.log(data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(3001, () => {
+  console.log("WebSocket server is running on port 3001");
+});
--- development/src/components/codingEditor.js
+++ development/src/components/codingEditor.js
@@ -1,12 +1,29 @@
-'use client';
-import MonacoEditor from 'react-monaco-editor/lib/editor';
-import { useTheme } from 'next-themes';
-import { useTabContext } from '@/composables/tabContext';
+"use client";
+import MonacoEditor from "react-monaco-editor/lib/editor";
+import { useTheme } from "next-themes";
+import { useTabContext } from "@/composables/tabContext";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 
 function CodingEditor() {
   const { theme, setTheme } = useTheme();
   const { items } = useTabContext();
+  const [codes, setCodes] = useState("");
+  const [room, setRoom] = useState("1234");
   const language = items.filter((e) => e.active)[0].ext;
+  const socket = io("http://localhost:3001");
+
+  useEffect(() => {
+    socket.emit("join-room", room);
+    socket.on("text-update", (data) => {
+      items.filter((e) => e.active)[0].code = data;
+      setCodes(data);
+    });
+
+    return () => {
+      socket.off("text-update");
+    };
+  }, [codes]);
 
   const editorDidMount = (editor, monaco) => {
     editor.focus();
@@ -14,6 +31,8 @@ function CodingEditor() {
 
   const onChange = (newValue, e) => {
     items.filter((e) => e.active)[0].code = newValue;
+    socket.emit("text-update", { room, newValue });
+    setCodes(newValue);
   };
 
   const options = {
@@ -25,15 +44,15 @@ function CodingEditor() {
       <MonacoEditor
         height="95vh"
         language={
-          language === '.js'
-            ? 'javascript'
-            : language === '.css'
-            ? 'css'
-            : language === '.html'
-            ? 'html'
-            : 'javascript'
+          language === ".js"
+            ? "javascript"
+            : language === ".css"
+            ? "css"
+            : language === ".html"
+            ? "html"
+            : "javascript"
         }
-        theme={theme == 'dark' ? 'vs-dark' : 'vs-light'}
+        theme={theme == "dark" ? "vs-dark" : "vs-light"}
         value={items.filter((e) => e.active)[0].code}
         options={options}
         onChange={onChange}
