de151fab7df39cc235dddd30a69d454e7549d782
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-13 19:41:34 +0000

    Jul 13, 2023, 8:41 PM

--- development/src/components/webcam_comp.js
+++ development/src/components/webcam_comp.js
@@ -142,7 +142,7 @@ export default function WebCamRecorder({
     await callPeer(sessionData.peerSessionId);
   }
 
-  
+  handle
 
   useEffect(() => {
     socket.emit("join-room", sessionData.peerSessionId);
