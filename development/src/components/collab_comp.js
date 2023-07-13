0a0e5f44f251847868add2328ec4a2c0b1db9d13
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-13 20:46:46 +0000

    Jul 13, 2023, 9:46 PM

--- development/src/components/collab_comp.js
+++ development/src/components/collab_comp.js
@@ -7,7 +7,7 @@ import { useStoreSession } from "@/composables/dbService";
 import { appAuth } from "@/composables/firebaseConfig/config";
 
 const Collab = ({ isCollabOn }) => {
-  const { sessionData } = useSessionContext();
+  const { sessionData, setSessioData } = useSessionContext();
   const { storeSession, getStoreSessionDetails } = useStoreSession();
   const user = appAuth.currentUser;
   useEffect(() => {
