378ddd932ce3afe92a63e43b72578c0d6461b9b9
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-13 20:56:30 +0000

    Jul 13, 2023, 9:56 PM

--- development/src/components/collab_comp.js
+++ development/src/components/collab_comp.js
@@ -14,8 +14,9 @@ const Collab = ({ isCollabOn }) => {
   const user = appAuth.currentUser;
   useEffect(() => {
     getStoreSessionDetails(sessionData.peerSessionId);
+   console.log(storeS)
     setStoreSessionData(storeSession)
-    console.log(sessionData);
+    ;
   }, [sessionData]);
 
   return (
