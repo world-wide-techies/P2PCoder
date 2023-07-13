cda17567f5b53fefb58a8ed42b69dc4e29cc0be8
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-13 20:04:49 +0000

    Jul 13, 2023, 9:04 PM

--- development/server.js
+++ development/server.js
@@ -33,7 +33,7 @@ const io = socketIO(server, {
 io.on("connection", (socket) => {
   console.log(`Socket Connected`, socket.id)
   socket.on("join-room", (roomId) => {
-    io.to(roomId).emit("user:joined", {roomId });
+    io.to(roomId).emit("user:joined", {roomId, id:socket.idd });
     socket.join(roomId);
     io.to(socket.id).emit("join-room", data)
     console.log(`Client joined room: ${roomId}`)
