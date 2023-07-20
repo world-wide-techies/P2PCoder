ba1f0b94479a0393e386b4457e91afc9bd5fe163
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-20 22:15:31 +0000

    Jul 20, 2023, 11:15 PM

--- development/server.js
+++ development/server.js
@@ -13,22 +13,22 @@ const io = socketIO(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  socket.on("join-room", (room) => {
-    socket.join(room);
-    console.log(`Client joined room: ${room}`);
-  });
+// io.on("connection", (socket) => {
+//   socket.on("join-room", (room) => {
+//     socket.join(room);
+//     console.log(`Client joined room: ${room}`);
+//   });
 
-  socket.on("text-update", (data) => {
-    // Broadcast the received update to all connected clients
-    io.to(data.room).emit("text-update", data.newValue);
-    console.log(data);
-  });
+//   socket.on("text-update", (data) => {
+//     // Broadcast the received update to all connected clients
+//     io.to(data.room).emit("text-update", data.newValue);
+//     console.log(data);
+//   });
 
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//   });
+// });
 
 server.listen(3001, () => {
   console.log("WebSocket server is running on port 3001");
