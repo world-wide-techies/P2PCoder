1d2964116fab0ba4375d980871b49748e34f1859
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-21 15:39:52 +0000

    Jul 21, 2023, 4:39 PM

--- development/src/components/webcam_comp.js
+++ development/src/components/webcam_comp.js
@@ -31,7 +31,7 @@ export default function WebCamRecorder({
   const [callAccepted, setCallAccepted] = useState(false);
   const [idToCall, setIdToCall] = useState(false);
   const [me, setMe] = useState(false);
-  const [otherUser, setOtherUser] = useState(false);
+  const otherUser = useRef()
 
   const [remoteSocketId, setRemoteSocketId] = useState(null);
   const room = 3;
