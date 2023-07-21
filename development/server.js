687264cce06f420ddfe03ac301988cf4ae9a2526
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-21 15:16:48 +0000

    Jul 21, 2023, 4:16 PM

--- development/server.js
+++ development/server.js
@@ -30,23 +30,41 @@ const io = socketIO(server, {
 //   });
 // });
 
-const rooms = {};
-
 io.on("connection", (socket) => {
-  socket.on("join room", (roomID) => {
-    if (rooms[roomID]) {
-      rooms[roomID].push(socket.id);
-    } else {
-      rooms[roomID] = [socket.id];
-    }
-    const otherUser = rooms[roomID].find((id) => id !== socket.id);
-    if (otherUser) {
-      socket.emit("other user", otherUser);
-      socket.to(otherUser).emit("user joined", socket.id);
-    }
+  console.log(`Socket Connected`, socket.id)
+  socket.on("join-room", (roomId) => {
+    io.to(roomId).emit("user:joined", {roomId, id:socket.id });
+    socket.join(roomId);
+    io.to(socket.id).emit("join-room", roomId)
+    console.log(`Client joined room: ${roomId}`)
+
+    ;
+  });
+
+  // socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+    console.log("Client disconnected");
+  });
+ 
+  socket.on("call-Peer", ({to, offer}) => {
+    io.to(to).emit("incoming-call", {
+      signal: offer,
+      from: socket.id,
+    });
+    console.log(socket.id);
+    console.log(offer);
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+
+    console.log(`Call accepted: ${data.signal}`);
   });
 });
 
 server.listen(3001, () => {
   console.log("WebSocket server is running on port 3001");
 });
+
