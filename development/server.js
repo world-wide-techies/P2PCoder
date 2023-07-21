d36eb5cd2f03613583acd9cfbc87a2e5fe220a37
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-21 15:17:17 +0000

    Jul 21, 2023, 4:17 PM

--- development/server.js
+++ development/server.js
@@ -1,70 +1,75 @@
-const http = require("http");
-const express = require("express");
-const cors = require("cors");
-const socketIO = require("socket.io");
-const app = express();
+// const http = require("http");
+// const express = require("express");
+// const cors = require("cors");
+// const socketIO = require("socket.io");
+// const app = express();
 
-const server = http.createServer(app);
-const io = socketIO(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  },
-});
+// const server = http.createServer(app);
+// const io = socketIO(server, {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   },
+// });
+
+// // io.on("connection", (socket) => {
+// //   socket.on("join-room", (room) => {
+// //     socket.join(room);
+// //     console.log(`Client joined room: ${room}`);
+// //   });
+
+// //   socket.on("text-update", (data) => {
+// //     // Broadcast the received update to all connected clients
+// //     io.to(data.room).emit("text-update", data.newValue);
+// //     console.log(data);
+// //   });
+
+// //   socket.on("disconnect", () => {
+// //     console.log("Client disconnected");
+// //   });
+// // });
 
 // io.on("connection", (socket) => {
-//   socket.on("join-room", (room) => {
-//     socket.join(room);
-//     console.log(`Client joined room: ${room}`);
+//   console.log(`Socket Connected`, socket.id)
+//   socket.on("join-room", (roomId) => {
+//     io.to(roomId).emit("user:joined", {roomId, id:socket.id });
+//     socket.join(roomId);
+//     io.to(socket.id).emit("join-room", roomId)
+//     console.log(`Client joined room: ${roomId}`)
+
+//     ;
 //   });
 
-//   socket.on("text-update", (data) => {
-//     // Broadcast the received update to all connected clients
-//     io.to(data.room).emit("text-update", data.newValue);
-//     console.log(data);
-//   });
+//   // socket.emit("me", socket.id);
 
 //   socket.on("disconnect", () => {
+//     socket.broadcast.emit("callEnded");
 //     console.log("Client disconnected");
+//   });
+ 
+//   socket.on("call-Peer", ({to, offer}) => {
+//     io.to(to).emit("incoming-call", {
+//       signal: offer,
+//       from: socket.id,
+//     });
+//     console.log(socket.id);
+//     console.log(offer);
+//   });
+
+//   socket.on("answerCall", (data) => {
+//     io.to(data.to).emit("callAccepted", data.signal);
+
+//     console.log(`Call accepted: ${data.signal}`);
 //   });
 // });
 
-io.on("connection", (socket) => {
-  console.log(`Socket Connected`, socket.id)
-  socket.on("join-room", (roomId) => {
-    io.to(roomId).emit("user:joined", {roomId, id:socket.id });
-    socket.join(roomId);
-    io.to(socket.id).emit("join-room", roomId)
-    console.log(`Client joined room: ${roomId}`)
+// server.listen(3001, () => {
+//   console.log("WebSocket server is running on port 3001");
+// });
 
-    ;
-  });
 
-  // socket.emit("me", socket.id);
 
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
-    console.log("Client disconnected");
-  });
- 
-  socket.on("call-Peer", ({to, offer}) => {
-    io.to(to).emit("incoming-call", {
-      signal: offer,
-      from: socket.id,
-    });
-    console.log(socket.id);
-    console.log(offer);
-  });
 
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
 
-    console.log(`Call accepted: ${data.signal}`);
-  });
-});
-
-server.listen(3001, () => {
-  console.log("WebSocket server is running on port 3001");
-});
 
