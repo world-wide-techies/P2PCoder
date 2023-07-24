008ee982e95c3421df4aca814a8ab5b8d18dfc0e
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-24 10:48:30 +0000

    Jul 24, 2023, 11:48 AM

--- development/src/composables/authContext.js
+++ development/src/composables/authContext.js
@@ -25,18 +25,4 @@ export const useAuthContext = () => {
   return useContext(AuthContext);
 };
 
-export const SessionContext = createContext();
 
-export const SessionContextProvider = ({ children }) => {
-  const [sessionData, setSessionData] = useState({});
-
-  return (
-    <SessionContext.Provider value={{ sessionData, setSessionData }}>
-      {children}
-    </SessionContext.Provider>
-  );
-};
-
-export const useSessionContext = () => {
-  return useContext(SessionContext);
-};
