44cabf0a081edefc38a1a9049a47fd556aed9727
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-24 11:05:07 +0000

    Jul 24, 2023, 12:05 PM

--- development/src/app/layout.js
+++ development/src/app/layout.js
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import './globals.css';
-import { nohemi } from '@/app/localFont';
-import { ThemeProvider } from 'next-themes';
-import { TabProvider } from '@/composables/tabContext';
-import { SessionContextProvider } from '@/composables/sessionContext';
+import "./globals.css";
+import { nohemi } from "@/app/localFont";
+import { ThemeProvider } from "next-themes";
+import { TabProvider } from "@/composables/tabContext";
+import { SessionContextProvider } from "@/composables/sessionContext";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${nohemi.variable} `}>
-        <SessionContextProvider>
-          <TabProvider>
-            <ThemeProvider attribute="class">{children}</ThemeProvider>
-          </TabProvider>
-        </SessionContextProvider>
-
-        <AuthProvider></AuthProvider>
+        <AuthProvider>
+          <SessionContextProvider>
+            <TabProvider>
+              <ThemeProvider attribute="class">{children}</ThemeProvider>
+            </TabProvider>
+          </SessionContextProvider>
+        </AuthProvider>
       </body>
     </html>
   );
