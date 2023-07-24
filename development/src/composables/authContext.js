12ab4d7b100f7a955bce22d721cfcd966eadbe2d
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-24 11:14:49 +0000

    Jul 24, 2023, 12:14 PM

--- development/src/composables/authContext.js
+++ development/src/composables/authContext.js
@@ -8,11 +8,11 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState("");
 
-  useEffect(() => {
-    appAuth.onAuthStateChanged((user) => {
-      setCurrentUser(user);
-    });
-  }, []);
+//   useEffect(() => {
+//     appAuth.onAuthStateChanged((user) => {
+//       setCurrentUser(user);
+//     });
+//   }, []);
 
   return (
     <AuthContext.Provider value={{ currentUser, setCurrentUser }}>
