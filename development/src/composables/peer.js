c937e707bcc16c4ee67ee5af02c10b945e3ff414
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-26 10:34:55 +0000

    Jul 26, 2023, 11:34 AM

--- development/src/composables/peer.js
+++ development/src/composables/peer.js
@@ -1,23 +1,3 @@
-function createPeer(userID) {
-  const peer = new RTCPeerConnection({
-    iceServers: [
-      {
-        urls: "stun:stun.stunprotocol.org",
-      },
-      {
-        urls: "turn:numb.viagenie.ca",
-        credential: "muazkh",
-        username: "webrtc@live.com",
-      },
-    ],
-  });
-
-  peer.onicecandidate = handleICECandidateEvent;
-  peer.ontrack = handleTrackEvent;
-  peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
-
-  return peer;
-}
 
 
 
