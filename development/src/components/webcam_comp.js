2abaac4617baa1419d31bef44625295b1f71980f
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-26 10:35:07 +0000

    Jul 26, 2023, 11:35 AM

--- development/src/components/webcam_comp.js
+++ development/src/components/webcam_comp.js
@@ -199,6 +199,28 @@ export default function WebCamRecorder({
     userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
 }
 
+function createPeer(userID) {
+  const peer = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: "stun:stun.stunprotocol.org",
+      },
+      {
+        urls: "turn:numb.viagenie.ca",
+        credential: "muazkh",
+        username: "webrtc@live.com",
+      },
+    ],
+  });
+
+  peer.onicecandidate = handleICECandidateEvent;
+  peer.ontrack = handleTrackEvent;
+  peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
+
+  return peer;
+}
+
+
 function handleNegotiationNeededEvent(userID) {
   peerRef.current.createOffer().then(offer => {
       return peerRef.current.setLocalDescription(offer);
