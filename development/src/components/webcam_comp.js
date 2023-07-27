a60f1ccc96e04f13575e18d78a41863970ce6161
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-27 14:51:32 +0000

    Jul 27, 2023, 3:51 PM

--- development/src/components/webcam_comp.js
+++ development/src/components/webcam_comp.js
@@ -11,7 +11,7 @@ export default function WebCamRecorder({
 }) {
   const { sessionData, setSessionData, storeSession, setStoreSession } =
     useSessionContext();
-  const socketRef = io.connect("http://localhost:3001");
+  const socket = io.connect("http://localhost:3001");
   const myVideoRef = useRef(null);
   const partnerVideoRef = useRef(null);
   const connectionRef = useRef(null);
@@ -34,7 +34,7 @@ export default function WebCamRecorder({
   const otherUser = useRef();
   const userStream = useRef();
   const peerRef = useRef();
-  //const socketRef = useRef();
+  const socketRef = useRef();
 
   const [remoteSocketId, setRemoteSocketId] = useState(null);
   const room = 3;
@@ -147,10 +147,9 @@ export default function WebCamRecorder({
   };
 
   useEffect(() => {
-    socketRef = io.connect("http://localhost:3001");
-    socketRef.emit("join room", sessionData.peerSessionId);
+    socketRef.current.emit("join room", sessionData.peerSessionId);
 
-    socketRef.on("other user", (userID) => {
+    socketRef.current.on("other user", (userID) => {
       callUser(userID);
       otherUser.current = userID;
     });
