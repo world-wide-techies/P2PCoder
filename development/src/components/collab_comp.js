86951f4d6158d151fa8edc4f9796dbdb8cd49940
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-07-31 00:22:04 +0000

    Jul 31, 2023, 1:22 AM

--- development/src/components/collab_comp.js
+++ development/src/components/collab_comp.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import CodingEditor from "./codingEditor";
 import { useSessionContext } from "@/composables/sessionContext";
 import { useEffect } from "react";
