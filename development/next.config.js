7a0f1712498fdfccb4030037261dfb4814aace55
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-02 14:54:28 +0000

    Aug 2, 2023, 3:54 PM

--- development/next.config.js
+++ development/next.config.js
@@ -2,6 +2,8 @@ const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { dev, isServer }) => {
+   if(!isServer)
+   
     config.plugins.push(new MonacoWebpackPlugin());
     return config;
   },
