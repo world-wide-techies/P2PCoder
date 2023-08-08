ec73bf9128504e8962a1c62b56c1b0c0a0bda8d4
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-08 18:13:51 +0100

    fixed webpack error

--- development/next.config.js
+++ development/next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
     if (!isServer) {
       config.plugins.push(new MonacoWebpackPlugin());
     }
+
+    config.externals = {
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    };
+
     return config;
   },
 };
